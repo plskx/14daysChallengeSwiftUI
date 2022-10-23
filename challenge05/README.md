94534daa9996ac461d5ed7da3dc0c256e9692a2f
Zai Santillan <zaisantillan14@gmail.com>
2022-10-24 03:05:05 +0800

    feat: add challenge 5

---
+++ challenge05/README.md
@@ -0,0 +1,5 @@
+# challenge 5 - War Card Game UI
+
+### Demo
+
+<img width="1680" alt="image" src="https://user-images.githubusercontent.com/57343545/197410649-659ee903-5bce-434c-8ddf-cd05c69621d6.png">
---
+++ challenge05/challenge05/ContentView.swift
@@ -0,0 +1,57 @@
+//  Date: 10/24/22
+//
+//  Author: Zai Santillan
+//  Github: @plskz
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("logo")
+            Spacer()
+            HStack {
+                Spacer()
+                Image("card3")
+                Spacer()
+                Image("card4")
+                Spacer()
+            }
+            Spacer()
+            Image("dealbutton")
+            Spacer()
+            
+            HStack {
+                Spacer()
+                VStack {
+                    Text("Player")
+                        .padding()
+                    Text("0")
+                }
+                
+                Spacer()
+                
+                VStack {
+                    Text("CPU")
+                        .padding()
+                    Text("0")
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .font(.title2)
+            
+            Spacer()
+            
+        }
+        .background(Image("background"))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
